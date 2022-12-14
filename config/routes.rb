041ddb200ced6ddbabba3c7b32d5167c0d1bc628
miyazato1872 <miyazato5997@gmail.com>
2022-12-14 21:10:48 +0900

    新規作成画面の表示
    新規チャットルームの作成ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update] #ここがeditとupdateになるのは暗記部分
+  resources :rooms, only: [:new, :create]  #上記同様。新規チャットルームの作成で動くアクションは「new」と「create」
+
 end
