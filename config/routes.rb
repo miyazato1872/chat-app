4f36b6d44c64da6092ed9b9184b0a280d5bd7d69
miyazato1872 <miyazato5997@gmail.com>
2022-12-14 23:07:09 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update] #ここがeditとupdateになるのは暗記部分
   resources :rooms, only: [:new, :create]  #上記同様。新規チャットルームの作成で動くアクションは「new」と「create」
 end
