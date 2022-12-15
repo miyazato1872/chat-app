3c92ee3396d2646934b2811649e8390fa7f118e2
miyazato1872 <miyazato5997@gmail.com>
2022-12-15 12:15:22 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]  #ここがeditとupdateになるのは暗記部分
-  resources :rooms, only: [:new, :create] do #上記同様。新規チャットルームの作成で動くアクションは「new」と「create」
+  resources :rooms, only: [:new, :create, :destroy] do #上記同様。新規チャットルームの作成で動くアクションは「new」と「create」
     resources :messages, only: [:index, :create]
   end
 end
