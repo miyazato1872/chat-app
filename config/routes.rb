8475dfe54e7388d6f29bf6ce636e615a9fd33d68
miyazato1872 <miyazato5997@gmail.com>
2022-12-15 11:00:34 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :users, only: [:edit, :update] #ここがeditとupdateになるのは暗記部分
-  resources :rooms, only: [:new, :create]  #上記同様。新規チャットルームの作成で動くアクションは「new」と「create」
+  resources :users, only: [:edit, :update]  #ここがeditとupdateになるのは暗記部分
+  resources :rooms, only: [:new, :create] do #上記同様。新規チャットルームの作成で動くアクションは「new」と「create」
+    resources :messages, only: [:index, :create]
+  end
 end
